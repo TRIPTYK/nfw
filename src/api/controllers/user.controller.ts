8478e1019198c14772e729fd9e14c6a0491a0fa1
konfer-be <info@konfer.be>
2019-01-22 09:36:01 +0100

    Compose repositories with Repository wich implements interface

--- src/api/controllers/user.controller.ts
+++ src/api/controllers/user.controller.ts
@@ -1,133 +1,147 @@
 import { Request, Response } from "express";
 import { User } from "./../models/user.model";
+import { UserRepository } from "./../repositories/user.repository";
+
 import * as HttpStatus from "http-status";
-import { omit } from "lodash";
-import { db } from "./../../config/environment.config";
 
-const userRepository = db.getRepository(User);
-
-/**
- * Get serialized user
- * 
- * @param {Object} req
- * @param {Object} res
- * 
- * @public
- */
-const get = (req: Request, res : Response) => res.json( req.locals.user.transform() );
-
-/**
- * Get logged in user info
- * 
- * @param {Object} req
- * @param {Object} res
- * 
- * @public
- */
-const loggedIn = (req: Request, res : Response) => res.json( req.user.transform() );
-
-/**
- * Create new user
- * 
- * @param {Object} req
- * @param {Object} res
- * @param {Function} next
- * 
- * @public
- */
-const create = async (req: Request, res : Response, next) => {
-  try {
-    const user = userRepository.create(req.body);
-    const savedUser = await userRepository.save(user);
-    res.status(HttpStatus.CREATED);
-    res.json(savedUser.transform());
-  } 
-  catch (error) {
-    next(User.checkDuplicateEmail(error));
-  }
-};
-
-/**
- * Replace existing user
- * 
- * @param {Object} req
- * @param {Object} res
- * @param {Function} next
- * 
- * @public
- */
-const replace = async (req: Request, res : Response, next) => {
-  try {
-    const user = await userRepository.findOne(req.params.userId);
-    userRepository.merge(user, req.body);
-    userRepository.save(user);
-    res.json( user.transform() );
-  } 
-  catch (error) {
-    next(User.checkDuplicateEmail(error));
-  }
-};
-
-/**
- * Update existing user
- * 
- * @param {Object} req
- * @param {Object} res
- * @param {Function} next
- * 
- * @public
- */
-const update = async (req: Request, res : Response, next) => {
-
-  try {
-    const user = await userRepository.findOne(req.params.userId);
-    userRepository.merge(user, req.body);
-    userRepository.save(user);
-    res.json( user.transform() );
-  }
-  catch(e) {
-    next(User.checkDuplicateEmail(e));
-  }
+export class UserController {
   
-};
+  /**
+   * 
+   */
+  repository : UserRepository ;
 
-/**
- * Get user list
- * 
- * @param {Object} req
- * @param {Object} res
- * @param {Function} next
- * 
- * @public
- */
-const list = async (req: Request, res : Response, next) => {
-  try {
-    const users = await User.list(req.query);
-    const transformedUsers = users.map(user => user.transform());
-    res.json(transformedUsers);
-  } 
-  catch (error) {
-    next(error);
+  /**
+   * 
+   */
+  constructor() { this.init(); }
+
+  /** */
+  async init() {
+    this.repository = new UserRepository();
   }
-};
 
-/**
- * Delete user
- * @public
- */
-const remove = (req: Request, res : Response, next) => {
+  /**
+   * Get serialized user
+   * 
+   * @param {Object} req
+   * @param {Object} res
+   * 
+   * @public
+   */
+  get(req: Request, res : Response) { res.json( req['locals'].user.transform() ) }
 
-  try {
-    const { user } = req.locals;
-    user
-      .remove()
-      .then( () => res.status(HttpStatus.NO_CONTENT).end() )
-      .catch( e => next(e) );
+  /**
+   * Get logged in user info
+   * 
+   * @param {Object} req
+   * @param {Object} res
+   * 
+   * @public
+   */
+  loggedIn (req: Request, res : Response) { res.json( req['user'].transform() ) }
+
+  /**
+   * Create new user
+   * 
+   * @param {Object} req
+   * @param {Object} res
+   * @param {Function} next
+   * 
+   * @public
+   */
+  async create (req: Request, res : Response, next) {
+    try {
+      const user = this.repository.getRepository().create(req.body);
+      const savedUser = await this.repository.getRepository().save(user);
+      res.status( HttpStatus.CREATED );
+      res.json( savedUser[0].transform() );
+    } 
+    catch (error) {
+      next( User.checkDuplicateEmail(error) );
+    }
   }
-  catch(e) {
-    next(e)
-  }
-  
-};
 
-export { get, loggedIn, create, replace, update, remove, list };
+  /**
+   * Replace existing user
+   * 
+   * @param {Object} req
+   * @param {Object} res
+   * @param {Function} next
+   * 
+   * @public
+   */
+  async replace (req: Request, res : Response, next) {
+    try {
+      const user = await this.repository.getRepository().findOne(req.params.userId);
+      this.repository.getRepository().merge(user, req.body);
+      this.repository.getRepository().save(user);
+      res.json( user.transform() );
+    } 
+    catch (error) {
+      next(User.checkDuplicateEmail(error));
+    }
+  }
+
+  /**
+   * Update existing user
+   * 
+   * @param {Object} req
+   * @param {Object} res
+   * @param {Function} next
+   * 
+   * @public
+   */
+  async update (req: Request, res : Response, next) {
+
+    try {
+      const user = await this.repository.getRepository().findOne(req.params.userId);
+      this.repository.getRepository().merge(user, req.body);
+      this.repository.getRepository().save(user);
+      res.json( user.transform() );
+    }
+    catch(e) {
+      next(User.checkDuplicateEmail(e));
+    }
+    
+  };
+
+  /**
+   * Get user list
+   * 
+   * @param {Object} req
+   * @param {Object} res
+   * @param {Function} next
+   * 
+   * @public
+   */
+  async list (req: Request, res : Response, next) {
+    try {
+      const users = await this.repository.list(req.query);
+      const transformedUsers = users.map(user => user.transform());
+      res.json(transformedUsers);
+    } 
+    catch (error) {
+      next(error);
+    }
+  }
+
+  /**
+   * Delete user
+   * @public
+   */
+  async remove (req: Request, res : Response, next) {
+
+    try {
+      const { user } = req['locals'];
+      user
+        .remove()
+        .then( () => res.status(HttpStatus.NO_CONTENT).end() )
+        .catch( e => next(e) );
+    }
+    catch(e) {
+      next(e)
+    }
+    
+  }
+}
--- src/api/repositories/user.repository.ts
+++ src/api/repositories/user.repository.ts
@@ -1,18 +1,115 @@
-import * as Moment from "moment-timezone";
-import * as Crypto from "crypto";
-
 import { User } from "./../models/user.model";
+import { Connection, Repository } from "typeorm";
+import { connection as DBConnection } from "./../../config/environment.config";
+import { omitBy, isNil } from "lodash";
+import { Moment } from "moment-timezone";
+import { IRepository } from "./../interfaces/IRepository.interface";
 
-import { connection } from "./../../config/environment.config";
+import * as Boom from "boom";
 
 /**
  * 
  */
-export class UserRepository {
+export class UserRepository implements IRepository {
 
-  constructor () { }
+  /** */
+  connection : Connection;
 
-  save() {
+  /**
+   * 
+   */
+  repository : Repository<User>;
+
+  /**
+   * 
+   */
+  constructor() { this.init() }
+
+  /**
+   * 
+   */
+  async init() {
+    this.connection = await DBConnection;
+    this.repository = this.connection.getRepository(User);
+  }
+
+  /**
+   * 
+   */
+  getRepository()  {
+    return this.repository;
+  }
+  
+  /**
+   * Get user
+   *
+   * @param {Number} id - The id of user
+   * 
+   * @returns {Object|Error}
+   */
+  async get(id: number) {
+
+    try {
+
+      let user = await this.repository.findOne(id); 
+
+      if (!user) 
+      {
+        throw Boom.notFound('User not found');
+      }
+
+      return user;
+    } 
+    catch (error) {
+      throw Boom.badImplementation(error.message);
+    }
+  }
+
+  /**
+   * 
+   */
+  list({ page = 1, perPage = 30, username, email, role }) {
+
+    const options = omitBy({ username, email, role }, isNil);
+
+    return this.repository
+      .createQueryBuilder("user")
+      .where("user.username = :username OR user.email = :email OR user.role = :role", { username: options.username, email : options.email, role: options.role })
+      .orderBy("user.createdAt", "DESC")
+      .offset(perPage * (page - 1))
+      .limit(perPage)
+      .execute();
+  }
+
+  /**
+   * Find user by email and tries to generate a JWT token
+   *
+   * @param {Object} - Payload data
+   * 
+   * @returns {Object|Error}
+   */
+  async findAndGenerateToken(options) {
+
+    const { email, password, refreshObject } = options;
+
+    if (!email) throw Boom.badRequest('An email is required to generate a token');
+    if (!password) throw Boom.badRequest('A password is required to authorize a token generating');
     
+    const user = await this.repository.findOne({ email });
+
+    if (!user) 
+    {
+      throw Boom.notFound('User not found');
+    }
+    else if (await user.passwordMatches(password) === false) 
+    {
+      throw Boom.unauthorized('Password must match to authorize a token generating');
+    }
+    else if (refreshObject && refreshObject.userEmail === email && Moment(refreshObject.expires).isBefore()) 
+    {
+      throw Boom.unauthorized('Invalid refresh token.');
+    }
+
+    return { user, accessToken: user.token() };
   }
 }
