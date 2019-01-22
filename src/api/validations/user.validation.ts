9e4f7ff4af7563e41bf3bdee5cb22a810bc45127
konfer-be <info@konfer.be>
2019-01-22 09:38:57 +0100

    Adapt validations to new model

--- src/api/validations/user.validation.ts
+++ src/api/validations/user.validation.ts
@@ -1,52 +1,57 @@
 import * as Joi from "joi";
-import * as User from "./../models/user.model";
+import { User } from "./../models/user.model";
 
-module.exports = {
-
-  // GET /v1/users
-  listUsers: {
-    query: {
-      page: Joi.number().min(1),
-      perPage: Joi.number().min(1).max(100),
-      name: Joi.string(),
-      email: Joi.string(),
-      role: Joi.string().valid(User.roles),
-    },
-  },
-
-  // POST /v1/users
-  createUser: {
-    body: {
-      email: Joi.string().email().required(),
-      password: Joi.string().min(6).max(128).required(),
-      name: Joi.string().max(128),
-      role: Joi.string().valid(User.roles),
-    },
-  },
-
-  // PUT /v1/users/:userId
-  replaceUser: {
-    body: {
-      email: Joi.string().email().required(),
-      password: Joi.string().min(6).max(128).required(),
-      name: Joi.string().max(128),
-      role: Joi.string().valid(User.roles),
-    },
-    params: {
-      userId: Joi.string().regex(/^[a-fA-F0-9]{24}$/).required(),
-    },
-  },
-
-  // PATCH /v1/users/:userId
-  updateUser: {
-    body: {
-      email: Joi.string().email(),
-      password: Joi.string().min(6).max(128),
-      name: Joi.string().max(128),
-      role: Joi.string().valid(User.roles),
-    },
-    params: {
-      userId: Joi.string().regex(/^[a-fA-F0-9]{24}$/).required(),
-    },
+// GET /v1/users
+const listUsers = {
+  query: {
+    page: Joi.number().min(1),
+    perPage: Joi.number().min(1).max(100),
+    name: Joi.string(),
+    email: Joi.string(),
+    role: Joi.string().valid(User.roles),
   },
 };
+
+// POST /v1/users
+const createUser = {
+  body: {
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).max(16).required(),
+    username: Joi.string().max(32),
+    lastname: Joi.string().max(32),
+    firstname: Joi.string().max(32),
+    role: Joi.string().valid(User.roles),
+  },
+};
+
+// PUT /v1/users/:userId
+const replaceUser = {
+  body: {
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).max(16).required(),
+    username: Joi.string().max(32),
+    lastname: Joi.string().max(32),
+    firstname: Joi.string().max(32),
+    role: Joi.string().valid(User.roles),
+  },
+  params: {
+    userId: Joi.string().regex(/^[a-fA-F0-9]{24}$/).required(),
+  },
+};
+
+// PATCH /v1/users/:userId
+const updateUser = {
+  body: {
+    email: Joi.string().email(),
+    password: Joi.string().min(8).max(16),
+    username: Joi.string().max(32),
+    lastname: Joi.string().max(32),
+    firstname: Joi.string().max(32),
+    role: Joi.string().valid(User.roles),
+  },
+  params: {
+    userId: Joi.string().regex(/^[a-fA-F0-9]{24}$/).required(),
+  },
+};
+
+export { listUsers, createUser, replaceUser, updateUser };
