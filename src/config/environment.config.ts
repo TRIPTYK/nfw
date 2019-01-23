05651ac9099014180ed8add384f560fced57b303
konfer-be <info@konfer.be>
2019-01-23 14:02:50 +0100

    Refactoring and typeorm constants

--- src/config/environment.config.ts
+++ src/config/environment.config.ts
@@ -1,5 +1,4 @@
 import * as Dotenv from "dotenv";
-import { TypeORMConfiguration } from "./typeorm.config";
 
 /**
  * Configure dotenv with variables.env file before app, to allow process.env accessibility in 
@@ -11,13 +10,21 @@ const environment = process.argv[2] && process.argv[2] === '--env' && process.ar
 
 Dotenv.config( { path : `${process.cwd()}/${environment}.env` } );
 
-const env = process.env.NODE_ENV;
-const port = process.env.PORT;
-const jwtSecret = process.env.JWT_SECRET;
+const env                   = process.env.NODE_ENV;
+const port                  = process.env.PORT;
+const jwtSecret             = process.env.JWT_SECRET;
 const jwtExpirationInterval = process.env.JWT_EXPIRATION_MINUTES;
-const connection = (async function(TypeORMConfiguration) { return await TypeORMConfiguration.getConnection() } ) (TypeORMConfiguration);
-const api = process.env.API_VERSION;
-const logs = process.env.NODE_ENV === 'production' ? 'combined' : 'development';
-const HTTPLogs = process.env.NODE_ENV === 'production' ? 'production' : 'dev';
+const api                   = process.env.API_VERSION;
+const logs                  = process.env.NODE_ENV === 'production' ? 'combined' : 'development';
+const HTTPLogs              = process.env.NODE_ENV === 'production' ? 'production' : 'dev';
+const typeorm               = { 
+  type: process.env.TYPEORM_TYPE, 
+  name: process.env.TYPEORM_NAME,
+  port: process.env.TYPEORM_PORT, 
+  host: process.env.TYPEORM_HOST, 
+  database: process.env.TYPEORM_DB, 
+  user: process.env.TYPEORM_USER, 
+  pwd: process.env.TYPEORM_PWD 
+};
 
-export { env, environments, port, jwtSecret, jwtExpirationInterval, connection, api, logs, HTTPLogs };
+export { env, environments, port, jwtSecret, jwtExpirationInterval, api, logs, HTTPLogs, typeorm };
