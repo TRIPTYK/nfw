6db0312bff169707a76b89dc88143a8b0796f7fc
konfer-be <info@konfer.be>
2019-01-23 14:01:10 +0100

    TypeORM connection

--- src/app.bootstrap.ts
+++ src/app.bootstrap.ts
@@ -1,11 +1,16 @@
 import { logger as Logger } from "./config/logger.config";
-import { port, env } from "./config/environment.config";
+import { port, env, typeorm } from "./config/environment.config";
+import { TypeORMConfiguration } from "./config/typeorm.config";
+
+/** Connection to Database server before app configuration */
+TypeORMConfiguration.connect().then( (connection) => {
+  Logger.info(`Connection to ${typeorm.type} server established on port ${typeorm.port} (${env})`);
+});
+
 import { app as App } from "./config/app.config";
 
 /** Listen to requests */
 App.listen( port, () => Logger.info(`HTTP server is now running on port ${port} (${env})`));
 
-/**
- * Exports Express
- */
+/** Exports Express */
 export { App };
