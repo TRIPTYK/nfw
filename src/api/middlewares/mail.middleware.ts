f67a8ca5407bd3e03f268ca33481b7228ea68fbc
konfer-be <info@konfer.be>
2019-01-24 14:43:25 +0100

    Implements mail sending with api-mail as service/middleware

---
+++ src/api/middlewares/mail.middleware.ts
@@ -0,0 +1,23 @@
+import { sendmail } from "./../services/mail-sender.service";
+import { Request, Response } from "express";
+import * as Boom from "boom";
+
+/**
+ * Wrap API email sending service.
+ * 
+ * @description Send an email before/after action, on a route. 
+ * 
+ * @param req Request
+ * @param res Response
+ * @param next Function
+ */
+const mail = async (req: Request, res: Response, next: Function) => {
+  try {
+    let response = await sendmail(req);
+    if(response.status !== 200) next( Boom.expectationFailed(response), false);
+    else next( null, response);
+  }
+  catch(e) { Boom.expectationFailed(e.message); }
+};
+
+export { mail };
