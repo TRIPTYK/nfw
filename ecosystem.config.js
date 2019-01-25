2fad6c2981e04607114495f88eb4c7c5876f097f
konfer-be <info@konfer.be>
2019-01-25 10:24:06 +0100

    Post deploy tasks

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -25,7 +25,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'git@github.com:repo.git',
       path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'npm install && npm run distify && tsc && pm2 reload ecosystem.config.js --env staging'
     },
     production : {
       user : 'node',
@@ -33,7 +33,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'git@github.com:repo.git',
       path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'npm install && npm run distify && tsc && pm2 reload ecosystem.config.js --env staging'
     }
   }
 };
