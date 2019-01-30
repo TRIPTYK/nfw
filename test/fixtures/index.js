6e379b52c263d802d5c168794ae9a62aed9598f7
konfer-be <info@konfer.be>
2019-01-30 09:20:11 +0100

    Use fixtures for testing, add refres-token response status test

---
+++ test/fixtures/index.js
@@ -0,0 +1,28 @@
+const uuid = require('uuid/v4');
+
+const chars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+const roles = ['admin', 'user', 'ghost'];
+
+const shuffle = (a) => {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a.join('');
+};
+
+exports.password = () => {
+  return (Math.random().toString(36).substring(2, 16) + Math.random().toString(36).substring(2, 16)).toLowerCase();
+};
+
+exports.user = (role, pwd) => {
+  return {
+    username: uuid().substr(0,32),
+    email: uuid().substr(0,32) + '@triptyk.be',
+    password: pwd || ( Math.random().toString(36).substring(2, 16) + Math.random().toString(36).substring(2, 16) ).toLowerCase().substr(0,8),
+    lastname: shuffle(chars).substr(0,8),
+    firstname: shuffle(chars).substr(0,8),
+    services: '{}',
+    role: role || roles[ Math.floor(Math.random() * 3) + 1 ]
+  };
+}
--- test/user.js
+++ test/user.js
@@ -1,30 +1,28 @@
 var request = require('supertest');
 var uuid = require('uuid/v4');
+var fixtures = require('./fixtures');
 
 describe("User CRUD", function () {
   
-  var server, agent, token, uid, id;
+  var server, agent, password, credentials, token, refreshToken, uid, id;
   var expect = require('chai').expect;
 
   before(function (done) {
 
-    let express = require('./../dist/app.bootstrap')
-    let credentials = {
-      username: 'triptyk',
-      email: 'steve@triptyk.eu',
-      password: 'e2q2mak7'
-    };
-
-    server = express.App;
-    agent = request.agent(server);
+    let express = require('./../dist/app.bootstrap');
+  
+    server      = express.App;
+    agent       = request.agent(server);
+    password    = fixtures.password();
+    credentials = fixtures.user('admin', password);
 
     agent
-      .post('/api/v1/auth/login')
+      .post('/api/v1/auth/register')
       .send(credentials)
-      .end(function(err, response){
-        expect(response.statusCode).to.equal(200);
+      .end(function(err, response) {
+        expect(response.statusCode).to.equal(201);
         token = response.body.token.accessToken;
-        uid = uuid().substr(0,32);
+        refreshToken = response.body.token.refreshToken;
         done();
       });
 
@@ -35,19 +33,11 @@ describe("User CRUD", function () {
     delete server;
   });
 
-  it('POST succeed as 201 on /api/v1/users', function (done) {
+  it('POST /api/v1/users succeed with 201', function (done) {
     agent
       .post('/api/v1/users')
       .set('Authorization', 'Bearer ' + token)
-      .send({
-        username: uid,
-        email: uid + '@triptyk.eu',
-        password: 'e2q2mak7',
-        services: "{}",
-        role: "admin",
-        firstname: uid.substr(0,8),
-        lastname: uid.substr(0,8)
-      })
+      .send(fixtures.user('admin'))
       .end(function(err, res) {
         expect(res.statusCode, 201);
         id = res.body.id;
@@ -55,41 +45,40 @@ describe("User CRUD", function () {
       }) 
   });
 
-  it('GET succeed as 200 on /api/v1/users/n', function (done) {
+  it('GET /api/v1/users/profile succeed with 200', function (done) {
+    agent
+      .get('/api/v1/users/profile')
+      .set('Authorization', 'Bearer ' + token)
+      .expect(200, done);
+  });
+
+  it('GET /api/v1/users/n succeed with 200', function (done) {
     agent
       .get('/api/v1/users/' + id)
       .set('Authorization', 'Bearer ' + token)
       .expect(200, done);
   });
 
-  it('PUT succeed as 200 on /api/v1/users/n', function (done) {
+  it('PUT /api/v1/users/n succeed with 200', function (done) {
     agent
       .put('/api/v1/users/' + id)
       .set('Authorization', 'Bearer ' + token)
-      .send({
-        username: uuid().substr(0,32),
-        email: uuid().substr(0,32) + '@triptyk.eu',
-        password: 'e2q2mak7',
-        services: "{}",
-        role: "admin",
-        firstname: uuid().substr(0,8),
-        lastname: uuid().substr(0,8)
-      })
+      .send(fixtures.user('user'))
       .expect(200, done);
   });
 
-  it('PATCH succeed as 200 on /api/v1/users/n', function (done) {
+  it('PATCH /api/v1/users/n succeed with 200', function (done) {
     agent
       .patch('/api/v1/users/' + id)
       .set('Authorization', 'Bearer ' + token)
       .send({
-        username: uuid().substr(0,32),
-        lastname: uuid().substr(0,8)
+        username: fixtures.user().username,
+        lastname: fixtures.user().lastname
       })
       .expect(200, done);
   });
 
-  it('DELETE succeed as 204 on /api/v1/users/n', function (done) {
+  it('DELETE /api/v1/users/n succeed with 204', function (done) {
     agent
       .delete('/api/v1/users/' + id)
       .set('Authorization', 'Bearer ' + token)
