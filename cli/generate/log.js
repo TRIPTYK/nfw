6fb7ed0cd15c95d08b15cad482c9a86be93f204a
DEFLORENNE Amaury <amaury.deflorenne@std.heh.be>
2019-02-12 13:44:20 +0100

    delete generated files command

--- cli/generate/log.js
+++ cli/generate/log.js
@@ -1,13 +1,13 @@
 var colors = require('colors/safe');
 
 exports.error = text => {
-  console.log(colors.red('x') + text);
+  console.log(`${colors.red('x')} ${text}`);
 };
 
 exports.warning = text => {
-  console.log(colors.yelllow('-') + text);
+  console.log(`${colors.yellow('-')} ${text}`);
 };
 
 exports.success = text => {
-  console.log(colors.green('v') + text);
+  console.log(`${colors.green('v')} ${text}`);
 };
