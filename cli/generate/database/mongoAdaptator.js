0d80ef833b797bb7674ee024f8c6c08ea03366bc
DEFLORENNE Amaury <amaury.deflorenne@std.heh.be>
2019-02-13 12:19:57 +0100

    mongoAdaptator with working getColumns

--- cli/generate/database/mongoAdaptator.js
+++ cli/generate/database/mongoAdaptator.js
@@ -1,20 +1,56 @@
 const databaseEnv = require('./databaseEnv');
 const dotenv = require('dotenv');
-const mongoose = require('mongoose');
+const { promisify } = require('es6-promisify');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
 const Log = require('../log');
+const url = `mongodb://${databaseEnv.user}:${databaseEnv.pwd}@${databaseEnv.host}:${databaseEnv.port}`;
 
-mongoose.connect(`mongodb://${databaseEnv.user}:${databaseEnv.pwd}@${databaseEnv.host}:${databaseEnv.port}/${databaseEnv.database}`,{useNewUrlParser: true},
-  error => {
-    if(error) Log.error(error);
-    Log.success('Connected to database');
+const client = new MongoClient();
+const Connect = promisify(MongoClient.connect);
+
+exports.getColumns = async (tableName) => {
+  const connection = await Connect(url,{ useNewUrlParser: true });
+  const db = connection.db(databaseEnv.database);
+  const collection = db.collection(tableName);
+  const FindOne = promisify(collection.findOne.bind(collection));
+
+  // make new Promise manually , and wait for mapReduce result
+  const reduceResult = await new Promise(async(resolve,reject) => {
+      collection.mapReduce(
+        function() {for (var key in this) { emit(key, this ); }}, // emit
+        function(key, stuff) { return null },
+        {
+            query:{},
+            out: { inline: 1 }
+        },
+        function(err, results) {
+            if (err) reject(err);
+            resolve(results);
+        }
+      );
+  });
+
+  if (Object.keys(reduceResult).length === 0) {
+    throw `Cannot detect data type of your mongodb table (${tableName})`;
   }
-);
 
-exports.connection = mongoose;
+  let columns = {};
 
-exports.getColumns = (tableName) => {
-    mongoose.tableName.find( {  } );
+  for (let variable in reduceResult) {
+    let column = reduceResult[variable]._id;
+    columns[column] = { $exists : true };
+  }
+
+  const columnsWithValues = await FindOne(columns);
+  columns = {};
+
+  for (let variable in reduceResult) {
+    let column = reduceResult[variable]._id;
+    let columnValue = columnsWithValues[column];
+    columns[column] = columnsWithValues[column].constructor.name;
+  }
+
+  return JSON.stringify(columns);
 };
-
-getColumns('');
