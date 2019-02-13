16cf1a010365806e2f6df4fc701e5a79fb2176c7
Dramix <romain.verliefden@std.heh.be>
2019-02-13 14:18:01 +0100

    HEY AMAURY

--- cli/generate/modelWrite.js
+++ cli/generate/modelWrite.js
@@ -19,20 +19,21 @@ const getTableInfo = async (dbType,tableName) => {
 exports._writeModel = async (table,dbType) =>{
        data = await getTableInfo(dbType,table);
        console.log(data);
+       console.log(data[0].Field);
        let capitalize  = table[0].toUpperCase() + table.substr(1);
        let lowercase   = table[0].toLowerCase() + table.substr(1);
        let file = await ReadFile(`${process.cwd()}/cli/generate/templates/modelHeader.txt`, 'utf-8');
        let output = file
       .replace(/{{ENTITY_LOWERCASE}}/ig, lowercase)
-      .replace(/{{ENTITY_CAPITALIZE}}/ig, capitalize); 
-       data.forEach(async col =>{
-            console.log(col);
-            let ColTemp = await ReadFile(`${process.cwd()}/cli/generate/templates/modelColumn.txt`, 'utf-8');
+      .replace(/{{ENTITY_CAPITALIZE}}/ig, capitalize);
+      let ColTemp = await ReadFile(`${process.cwd()}/cli/generate/templates/modelColumn.txt`, 'utf-8');
+      data.forEach(async col =>{
+           console.log(col.Field)
             let colFinal = ColTemp
             .replace(/{{ROW_NAME}}/ig, col.Field)
             .replace(/{{ROW_DEFAULT}}/ig, col.Default) 
             .replace(/{{ROW_TYPE}}/ig, col.Type);
-            output += colFinal ;
+            output += colFinal +"\n\n" ;
       });
       console.log(output);
       
