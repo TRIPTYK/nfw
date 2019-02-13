762fdc1b7245990e49d0a33c68aa2dc91952d5d0
DEFLORENNE Amaury <amaury.deflorenne@std.heh.be>
2019-02-13 09:16:14 +0100

    Env file vars export

--- cli/generate/database/mongoAdaptator.js
+++ cli/generate/database/mongoAdaptator.js
@@ -1,7 +1,20 @@
+const databaseEnv = require('./databaseEnv');
+const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+
+const Log = require('../log');
+
+mongoose.connect(`mongodb://${databaseEnv.user}:${databaseEnv.pwd}@${databaseEnv.host}:${databaseEnv.port}/${databaseEnv.database}`,{useNewUrlParser: true},
+  error => {
+    if(error) Log.error(error);
+    Log.success('Connected to database');
+  }
+);
 
 exports.connection = mongoose;
 
 exports.getColumns = (tableName) => {
-
+    mongoose.tableName.find( {  } );
 };
+
+getColumns('');
