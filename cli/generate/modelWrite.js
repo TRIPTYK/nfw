2132a0e912595f454d59b9175f7e9bac4203ac70
Dramix <romain.verliefden@std.heh.be>
2019-02-14 16:10:32 +0100

    no banana

--- cli/generate/modelWrite.js
+++ cli/generate/modelWrite.js
@@ -34,7 +34,7 @@ const haveLenght = (data) =>{
         let better = type[1].replace(')',"") ;
         return "length : "+better+",";
     }else{
-        return "\b";
+        return '';
     }
 }
 
@@ -51,14 +51,13 @@ exports._writeModel = async (table,dbType) =>{
     let capitalize  = table[0].toUpperCase() + table.substr(1);
     let lowercase   = table[0].toLowerCase() + table.substr(1);
     let path = `${process.cwd()}/src/api/models/${lowercase}.model.ts`
-    let entityExists = await Exists(path);
     let file = await ReadFile(`${process.cwd()}/cli/generate/templates/modelHeader.txt`, 'utf-8');
     let ColTemp = await ReadFile(`${process.cwd()}/cli/generate/templates/modelColumn.txt`, 'utf-8');
     data = await getTableInfo(dbType,table);
     console.log(data);
     console.log(data[0].Field);
     var Entities;
-    data.forEach(async col =>{
+    await data.forEach(async col =>{
         if(col.Field === "id"){
             return;
         }
