619eab69c3d1ebe8dbe9dbf7cbeb9bc1b46781dc
DEFLORENNE Amaury <amaury.deflorenne@std.heh.be>
2019-02-15 13:27:07 +0100

    tests

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,17 +5,18 @@ import { typeorm as TypeORM } from "./environment.config";
 import { User } from "../api/models/user.model";
 import { RefreshToken } from "../api/models/refresh-token.model";
 import { Document } from "../api/models/document.model";
+import { Recipe } from "../api/models/recipe.model";
 
 /**
  * Define TypeORM default configuration
- * 
+ *
  * @inheritdoc https://http://typeorm.io
  */
 class TypeORMConfiguration {
   private static connection : Connection;
   constructor () { }
-  public static async connect() 
-  { 
+  public static async connect()
+  {
     this.connection = await createConnection({
       type: "mysql",
       name: TypeORM.name,
@@ -24,7 +25,7 @@ class TypeORMConfiguration {
       username: TypeORM.user,
       password: TypeORM.pwd,
       database: TypeORM.database,
-      entities: [ User, RefreshToken, Document],
+      entities: [ User, RefreshToken, Document , Recipe], // ^\s*entities.*$
       synchronize: true,
       logging: false
     });
