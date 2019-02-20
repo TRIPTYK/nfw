91da168d7edf2c9c0cbb1e0b6c108603056d6b57
DEFLORENNE Amaury <amaury.deflorenne@std.heh.be>
2019-02-20 16:34:38 +0100

    unresolved merge conflict

--- cli/generate/generateFromDB.js
+++ cli/generate/generateFromDB.js
@@ -7,25 +7,15 @@ const modelWrite = require ('./modelWrite')
  * @description get all table from DB then call writeModel method for each table in the database
  *
  */
-<<<<<<< HEAD
-_generateFromDB = async () =>{
-    tables = await SqlAdaptator.getTables();
-    tablesIn = await SqlAdaptator.getTablesInName();
-    for(let j = 0;j<tables.length;j++){
-        await modelWrite.writeModel(tables[j][tablesIn],"sql");
-    };
-=======
 const _generateFromDB = async () =>{
     let p_tables = SqlAdaptator.getTables();
     let p_tablesIn = SqlAdaptator.getTablesInName();
 
     let [tables,tablesIn] = await Promise.all([p_tables,p_tablesIn]);
 
-    tables.forEach(table => {
-        modelWrite.writeModel(table[tablesIn],"sql");
-    });
->>>>>>> 02a06b84f22f268f439ada9bf523661f8970e681
+    for(let j = 0;j<tables.length;j++){
+        await modelWrite.writeModel(tables[j][tablesIn],"sql");
+    };
 }
-
 
 _generateFromDB();
