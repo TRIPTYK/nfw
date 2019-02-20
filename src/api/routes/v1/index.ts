7ee6aa4f1a75dea66d97076922df1c0675af7a1e
Dramix <romain.verliefden@std.heh.be>
2019-02-20 16:59:51 +0100

    im a mess2

--- src/api/routes/v1/index.ts
+++ src/api/routes/v1/index.ts
@@ -3,7 +3,6 @@ import { Request, Response } from "express";
 import { router as AuthRouter } from "./auth.route";
 import { router as UserRouter } from "./user.route";
 import { router as DocumentRouter } from "./document.route";
-import { router as OtterRouter } from "./otter.route";
 
 
 const router = Router();
@@ -36,11 +35,6 @@ router.use('/users/', UserRouter);
  */
 router.use('/documents/', DocumentRouter);
 
-
-/**
- * Otter routes 
- */
-router.use('/otters/', OtterRouter);
 
 export { router }
 
