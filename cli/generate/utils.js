be7801c07d7607fc1c094807c5e7236ad251a828
DEFLORENNE Amaury <amaury.deflorenne@std.heh.be>
2019-02-21 10:13:50 +0100

    Doc of utils

--- cli/generate/utils.js
+++ cli/generate/utils.js
@@ -1,12 +1,12 @@
 /**
- * Require the library FS
+ * Requires
  */
 const FS = require('fs');
-const _ = require('lodash');
+const readline = require('readline');
 
 /**
  * @description : count the lines of a file
- * @param {*} path
+ * @param {string} path
  */
 exports.countLines = (path) => {
   let count = 0;
@@ -28,8 +28,10 @@ exports.countLines = (path) => {
   });
 };
 
-const readline = require('readline');
-
+/**
+ * @description prompt a question and wait for a response
+ * @param {string} question the question to ask
+ */
 exports.prompt = (question) => {
   const rl = readline.createInterface({
       input: process.stdin,
@@ -44,12 +46,35 @@ exports.prompt = (question) => {
   });
 }
 
-exports.isImportPresent = (string,imprt) => {
-  let doesMatch = string.match(new RegExp(`import.*${imprt}.*;`,'')) !== null;
-  return doesMatch;
-};
+/**
+ * @description check if import is present in string
+ * @param {string} string
+ * @param {string} imprt import name
+ */
+exports.isImportPresent = (string,imprt) => (string.match(new RegExp(`import.*${imprt}.*;`,'')) !== null);
 
+/**
+ * @description remove import from string
+ * @param {string} string
+ * @param {string} imprt import name
+ */
 exports.removeImport = (string,imprt) => string.replace(new RegExp(`\n?import.*${imprt}.*;`,"g"),"");
+
+/**
+ * @description replace text to the first empty line of string
+ * @param {string} string
+ * @param {string} by text to replace by
+ */
 exports.writeToFirstEmptyLine = (string,by = "") => string.replace(/^\s*$/m,by);
+
+/**
+ * @description remove blank lines from text
+ * @param {string} string
+ */
 exports.removeEmptyLines = (string) => string.replace(/\n?^\s*$/gm,"");
+
+/**
+ * @description capitalize first letter of String
+ * @param {string} entity
+ */
 exports.capitalizeEntity = (entity) => entity[0].toUpperCase() + entity.substr(1);
