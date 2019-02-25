bc36a2158b2365fc251c62be1f9da205cdd10eb4
Dramix <romain.verliefden@std.heh.be>
2019-02-25 09:30:32 +0100

    generateFromDB is now module

--- cli/generate/generateFromDB.js
+++ cli/generate/generateFromDB.js
@@ -1,5 +1,6 @@
 const SqlAdaptator = require ('./database/sqlAdaptator');
-const modelWrite = require ('./modelWrite')
+const modelWrite = require ('./modelWrite');
+const index = require ('./index')
 
 const noGenerate = ['user','document','refresh_token','migration_table'];
 /**
@@ -14,8 +15,11 @@ const _generateFromDB = async () =>{
     let [tables,tablesIn] = await Promise.all([p_tables,p_tablesIn]);
 
     for(let j = 0;j<tables.length;j++){
-        if(!noGenerate.includes(tables[j][tablesIn])) await modelWrite('db',tables[j][tablesIn]);
-    };
+        if(!noGenerate.includes(tables[j][tablesIn])){
+             await modelWrite('db',tables[j][tablesIn]);
+             index(tables[j][tablesIn],'crud');
+            }    
+   };
 }
 
-_generateFromDB();
+module.exports=_generateFromDB();
