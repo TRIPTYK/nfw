87b35d8e2ffb63e036946cb0aeab69b4cfaadcdd
DEFLORENNE Amaury <amaury.deflorenne@std.heh.be>
2019-03-01 12:26:49 +0100

    added new imports from templates

--- src/api/models/refresh-token.model.ts
+++ src/api/models/refresh-token.model.ts
@@ -1,17 +1,17 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToOne, JoinColumn } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, OneToOne, JoinColumn , CreateDateColumn , UpdateDateColumn } from "typeorm";
 import { User } from "./../models/user.model";
 
 @Entity()
 export class RefreshToken {
 
   /**
-   * 
-   * @param token 
-   * @param user 
-   * @param expires 
+   *
+   * @param token
+   * @param user
+   * @param expires
    */
-  constructor(token: String, user: User, expires: Date) 
-  { 
+  constructor(token: String, user: User, expires: Date)
+  {
     this.token = token;
     this.expires = expires;
     this.user = user;
@@ -23,7 +23,7 @@ export class RefreshToken {
   @Column()
   token: String;
 
-  @OneToOne(type => User, { 
+  @OneToOne(type => User, {
     eager : true,
     onDelete: "CASCADE" // Remove refresh-token when user is deleted
    })
