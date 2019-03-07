a80cbe886040cac61f71048e1725ce86041b7d49
DEFLORENNE Amaury <amaury.deflorenne@std.heh.be>
2019-03-07 14:28:54 +0100

    updated serializers

--- src/api/serializers/document.serializer.ts
+++ src/api/serializers/document.serializer.ts
@@ -1,15 +1,13 @@
 import { BaseSerializer } from "./base.serializer";
 
 export class DocumentSerializer extends BaseSerializer {
+  public static withelist : Array<String> = ['fieldname','filename','path','mimetype','size','user','createdAt'];
 
-  constructor() { super('documents', [ 
-    'fieldname', 
-    'filename', 
-    'path', 
-    'mimetype', 
-    'size', 
-    'user', 
-    'createdAt' 
-  ]) };
+  constructor() {
+    super('documents',{
+      id : 'id',
+      attributes : DocumentSerializer.withelist
+    });
+  };
 
 }
--- src/api/serializers/user.serializer.ts
+++ src/api/serializers/user.serializer.ts
@@ -1,16 +1,16 @@
 import { BaseSerializer } from "./base.serializer";
+import { RecipeSerializer } from "./recipe.serializer";
 
 export class UserSerializer extends BaseSerializer {
+  public static withelist : Array<String> = ['username','email','services','documents','recipes','firstname','lastname','role','createdAt'];
 
   constructor() {
-
-
     super('users',{
       id: 'id',
-      attributes : ['username','email','services','documents','recipes','firstname','lastname','role','createdAt'],
+      attributes : UserSerializer.withelist,
       recipes : {
         ref: 'id',
-        attributes : ['title']
+        attributes : RecipeSerializer.withelist
       }
     })
   };
