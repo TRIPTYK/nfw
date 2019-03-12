427675d41ae87a14b9c27368084a4d30db622c04
DEFLORENNE Amaury <amaury.deflorenne@std.heh.be>
2019-03-12 13:27:36 +0100

    changed JSON API request handling

--- src/api/controllers/base.controller.ts
+++ src/api/controllers/base.controller.ts
@@ -8,7 +8,7 @@ abstract class BaseController {
 
   /**
    * Store the TypeORM current connection to database
-   * 
+   *
    * @property Connection
    * @protected
    */
@@ -19,6 +19,88 @@ abstract class BaseController {
    * Retrieve database connection, and store it into connection
    */
   constructor() { this.connection = getConnection(TypeORM.name); }
+
+
+  /**
+   * @description : Handle request and transform to SelectQuery , conform to JSON-API specification : https://jsonapi.org/format/ (VERSION 1.0)
+   */
+  protected static JSONAPIRequest(query : any) : any {
+    const splitAndFilter = (string : string) => string.split(',').filter(string => string != '');  //split parameters and filter empty strings
+
+    let options = {
+      relations : [],
+      order : {},
+      take : null,
+      skip : null
+    };
+
+    /**
+     * Check if include parameter exists
+     * An endpoint MAY also support an include request parameter to allow the client to customize which related resources should be returned.
+     * @ref : https://jsonapi.org/format/#fetching-includes
+     */
+    if (query.include)
+    {
+      let includes = splitAndFilter(query.include);
+      includes.forEach( (include: string) => options.relations.push(include) );
+    }
+
+
+    /**
+     * Check if fields parameter exists
+     * A client MAY request that an endpoint return only specific fields in the response on a per-type basis by including a fields[TYPE] parameter.
+     * @ref : https://jsonapi.org/format/#fetching-sparse-fieldsets
+     */
+    if (query.fields)
+    {
+      if (typeof query.fields === "string") {
+        splitAndFilter(query.fields)
+          .forEach(elem => options["select"].push(elem) );
+      }else{
+        for (let property in query.fields) {
+            splitAndFilter(query.fields[property])
+              .forEach(elem => options["select"].push(`${property}.${elem}`));
+        }
+      }
+    }
+
+
+    /**
+     * Check if sort parameter exists
+     * A server MAY choose to support requests to sort resource collections according to one or more criteria (“sort fields”).
+     * @ref : https://jsonapi.org/format/#fetching-sorting
+     */
+    if (query.sort)
+    {
+       let sortFields = splitAndFilter(query.sort); //split parameters and filter empty strings
+
+       // need to use SqlString.escapeId in order to prevent SQL injection on orderBy()
+       sortFields.forEach((field : string) => {
+        if (field[0] == "-") {  // JSON-API convention , when sort field starts with '-' order is DESC
+          options.order[field.substr(1)] = "DESC"; //
+        }else{
+          options.order[field] = "ASC";
+        }
+       });
+    }
+
+
+    /**
+     * Check if pagination is enabled
+     * A server MAY choose to limit the number of resources returned in a response to a subset (“page”) of the whole set available.
+     * @ref : https://jsonapi.org/format/#fetching-pagination
+     */
+    if (query.page && query.page.number && query.page.size)
+    {
+      let number = query.page.number;
+      let size = query.page.size;
+
+      options.skip =  (number - 1) * size ;
+      options.take =  size ;
+    }
+
+    return options;
+  }
 
 };
 
--- src/api/controllers/document.controller.ts
+++ src/api/controllers/document.controller.ts
@@ -29,7 +29,7 @@ class DocumentController extends BaseController {
   public async list (req: Request, res : Response, next: Function) {
     try {
       const repository = getCustomRepository(DocumentRepository);
-      const documents = await repository.list(req.query);
+      const documents = await repository.find(BaseController.JSONAPIRequest(req.query));
       res.json( new DocumentSerializer().serialize(documents) );
     }
     catch (e) { next(e); }
@@ -66,7 +66,7 @@ class DocumentController extends BaseController {
   public async get(req: Request, res: Response, next: Function) {
     try {
       const documentRepository = getRepository(Document);
-      let document = await documentRepository.findOneOrFail(req.params.documentId);
+      let document = await documentRepository.findOneOrFail(BaseController.JSONAPIRequest(req.query));
       res.json(document.whitelist());
     }
     catch(e) { next(Boom.expectationFailed(e.message)); }
