895bcfe20d5cfb30d28fb75ec518136c65396ce4
DEFLORENNE Amaury <amaury.deflorenne@std.heh.be>
2019-03-18 13:17:57 +0100

    documents are not included in test

--- src/api/serializers/user.serializer.ts
+++ src/api/serializers/user.serializer.ts
@@ -3,6 +3,9 @@ import { DocumentSerializer } from "./document.serializer";
 import { api, env , port, url } from "../../config/environment.config";
 import { Request } from "express";
 import { SerializerParams } from "./serializerParams";
+import { User } from "../models/user.model";
+import { getRepository } from "typeorm";
+import { UserRepository } from "../repositories/user.repository";
 
 
 export class UserSerializer extends BaseSerializer {
@@ -12,26 +15,29 @@ export class UserSerializer extends BaseSerializer {
     let params = new SerializerParams();
 
     params
-    .setAttributes(UserSerializer.withelist)
-    .loadRelation(new DocumentSerializer())
-    .setDataLinks({
-      self : (dataSet,data) => `${url}/api/${api}/${this.type}/${data.id}`
-    });
-
-    if (request && request.query.page.number && totalCount) {
-      const page = parseInt(request.query.page.number);
-      const size = request.query.page.size;
-      const baseUrl = `${url}/api/${api}`;
-      const max = Math.ceil(totalCount / size);
-
-      params.setTopLevelLinks({
-        self : () =>  `${baseUrl}/${this.type}${request.url}`,
-        next : () => `${baseUrl}/${this.type}${this.replacePage(request.url,page + 1 > max ? max  : page + 1)}`,
-        prev : () => `${baseUrl}/${this.type}${this.replacePage(request.url,page - 1 <   1 ? page : page - 1)}`,
-        last : () => `${baseUrl}/${this.type}${this.replacePage(request.url,max)}` ,
-        first : () =>  `${baseUrl}/${this.type}${this.replacePage(request.url,1)}`
+      .setAttributes(UserSerializer.withelist)
+      .addRelation('documents',{
+        ref : "id",
+        attributes : DocumentSerializer.withelist
       })
-    }
+      .setDataLinks({
+        self : (dataSet,data) => `${url}/api/${api}/${this.type}/${data.id}`
+      });
+
+      if (request && request.query.page && totalCount) {
+        const page = parseInt(request.query.page.number);
+        const size = request.query.page.size;
+        const baseUrl = `${url}/api/${api}`;
+        const max = Math.ceil(totalCount / size);
+
+        params.setTopLevelLinks({
+          self : () =>  `${baseUrl}/${this.type}${request.url}`,
+          next : () => `${baseUrl}/${this.type}${this.replacePage(request.url,page + 1 > max ? max  : page + 1)}`,
+          prev : () => `${baseUrl}/${this.type}${this.replacePage(request.url,page - 1 <   1 ? page : page - 1)}`,
+          last : () => `${baseUrl}/${this.type}${this.replacePage(request.url,max)}` ,
+          first : () =>  `${baseUrl}/${this.type}${this.replacePage(request.url,1)}`
+        })
+      }
 
     super('users',params);
   };
