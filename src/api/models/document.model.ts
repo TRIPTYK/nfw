60f7e3730e275103bbe4f9ca89339e194d41f7a5
DEFLORENNE Amaury <amaury.deflorenne@std.heh.be>
2019-03-18 13:17:57 +0100

    documents are not included in test

--- src/api/models/document.model.ts
+++ src/api/models/document.model.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne , CreateDateColumn , UpdateDateColumn } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne , CreateDateColumn , UpdateDateColumn, OneToMany } from "typeorm";
 import { User } from "./user.model";
 import { DateUtils } from "typeorm/util/DateUtils";
 import { mimeTypes } from "./../enums/mime-type.enum";
