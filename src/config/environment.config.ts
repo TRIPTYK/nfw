a4aa259067b134d5c8ce80918a930d2b9d186b79
DEFLORENNE Amaury <amaury.deflorenne@std.heh.be>
2019-04-02 15:38:42 +0100

    fix CROS

--- src/config/environment.config.ts
+++ src/config/environment.config.ts
@@ -2,13 +2,13 @@ import * as Dotenv from "dotenv";
 import * as fs from 'fs';
 
 /**
- * Configure dotenv with variables.env file before app, to allow process.env accessibility in 
+ * Configure dotenv with variables.env file before app, to allow process.env accessibility in
  * app.js
  */
 const environments = { DEVELOPMENT : 'DEVELOPMENT' , STAGING : 'STAGING', PRODUCTION : 'PRODUCTION', TEST : 'TEST' };
 
 const environment = process.argv[2] && process.argv[2] === '--env' && process.argv[3] && environments.hasOwnProperty(process.argv[3].toUpperCase()) ? process.argv[3] : 'development';
-
+let authorized : any = process.env.AUTHORIZED.trim();
 
 const envConfig = Dotenv.parse(fs.readFileSync(`${process.cwd()}/${environment}.env`));
 for (let k in envConfig) {
@@ -20,20 +20,20 @@ Dotenv.config( { path : `${process.cwd()}/${environment}.env` } );
 const env                   = process.env.NODE_ENV;
 const port                  = process.env.PORT;
 const url                   = process.env.URL;
-const authorized            = process.env.AUTHORIZED.trim().split(',');
+authorized                  = authorized === "*" ? true : authorized.split(',');
 const jwtSecret             = process.env.JWT_SECRET;
 const jwtExpirationInterval = process.env.JWT_EXPIRATION_MINUTES;
 const api                   = process.env.API_VERSION;
 const logs                  = process.env.NODE_ENV === 'production' ? 'combined' : 'development';
 const HTTPLogs              = process.env.NODE_ENV === 'production' ? 'production' : 'dev';
-const typeorm               = { 
-  type: process.env.TYPEORM_TYPE, 
+const typeorm               = {
+  type: process.env.TYPEORM_TYPE,
   name: process.env.TYPEORM_NAME,
-  port: process.env.TYPEORM_PORT, 
-  host: process.env.TYPEORM_HOST, 
-  database: process.env.TYPEORM_DB, 
-  user: process.env.TYPEORM_USER, 
-  pwd: process.env.TYPEORM_PWD 
+  port: process.env.TYPEORM_PORT,
+  host: process.env.TYPEORM_HOST,
+  database: process.env.TYPEORM_DB,
+  user: process.env.TYPEORM_USER,
+  pwd: process.env.TYPEORM_PWD
 };
 const jimp                  = {
   isActive: parseInt(process.env.JIMP_IS_ACTIVE),
