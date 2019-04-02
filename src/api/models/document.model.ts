fc8f47c657530f29e718bc94c32d304ed45eb870
Snorkell <samgaming29@live.fr>
2019-04-02 11:03:30 +0200

    Fixed error handler and some issues

--- src/api/models/document.model.ts
+++ src/api/models/document.model.ts
@@ -1,6 +1,5 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne , CreateDateColumn , UpdateDateColumn, Timestamp } from "typeorm";
 import { User } from "./user.model";
-import { DateUtils } from "typeorm/util/DateUtils";
 import { mimeTypes } from "./../enums/mime-type.enum";
 import { documentTypes } from "./../enums/document-type.enum";
 import { DocumentSerializer } from "./../serializers/document.serializer";
