e9b84ec394218cfb4dea447150964a1aa85ef681
Snorkell <samgaming29@live.fr>
2019-04-15 11:33:27 +0200

    Add random generating functions

--- src/api/routes/v1/user.route.ts
+++ src/api/routes/v1/user.route.ts
@@ -44,7 +44,7 @@ router
    * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
    * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
    */
-  .get(/*authorize([ADMIN]),*/validate(listUsers), userController.list)
+  .get(authorize([ADMIN]),validate(listUsers), userController.list)
 
   /**
    * @api {post} v1/users Create User
