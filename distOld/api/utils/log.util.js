4ab3b95c5cc0cb3c8f966fb4daaa8328e9bb5050
Snorkell <samgaming29@live.fr>
2019-04-15 14:07:14 +0200

    Remove unused files

--- distOld/api/utils/log.util.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const NodeUtil = require("util");
-const fullLog = (element) => console.log(NodeUtil.inspect(element, { showHidden: true, depth: null, colors: true }));
-exports.fullLog = fullLog;
-const shadowLog = (element) => console.log(NodeUtil.inspect(element, { showHidden: true, depth: 1, colors: true }));
-exports.shadowLog = shadowLog;
