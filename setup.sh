1a7192f937ba13a11a8d8b013d2d1a6cb979ac7b
Gilles Bertrand <gilles@triptyk.eu>
2019-07-09 08:59:43 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,4 @@ echo "Installing NFW ......"
 echo "What folder name should we set ?"
 read foldername
 git clone https://github.com/TRIPTYK/nfw.git $foldername
-echo "setup folder"
-cd . $foldername
-rm -rf .git
-yarn install
+
