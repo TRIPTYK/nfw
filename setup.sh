10f391e2b1770794d35b7ec453a89dc28b8ec9d6
Gilles Bertrand <gilles@triptyk.eu>
2019-07-09 08:43:12 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
-git clone https://github.com/TRIPTYK/nfw.git
-cd nfw2
+echo "Installing NFW ......"
+echo "What folder name should we set ?"
+read foldername
+git clone https://github.com/TRIPTYK/nfw.git $foldername
+cd nfw
 rm -rf .git
 yarn install
