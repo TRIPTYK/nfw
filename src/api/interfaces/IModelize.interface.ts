bd2e4ded243e858e9cbef22efdc0d08e75b06269
amaury.deflorenne <amaury.deflorene@std.heh.be>
2019-07-23 10:01:49 +0200

    implement interface for later

--- src/api/interfaces/IModelize.interface.ts
+++ src/api/interfaces/IModelize.interface.ts
@@ -2,7 +2,7 @@
  * Define required members for Model's
  */
 interface IModelize {
-    whitelist: Function
+
 }
 
 export {IModelize};
--- src/api/models/base.model.ts
+++ src/api/models/base.model.ts
@@ -1,4 +1,6 @@
-export abstract class BaseModel {
+import {IModelize} from "../interfaces/IModelize.interface";
+
+export abstract class BaseModel implements IModelize {
     public constructor(payload: Object = {}) {
         Object.assign(this, payload);
     }
