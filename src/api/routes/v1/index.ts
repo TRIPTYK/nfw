cda8c3d5e3d854d7e9edb2080d6b2d93c8aad64b
AmauryD <deflorenne.amaury@gmail.com>
2020-01-14 15:15:08 +0100

    remove setting

--- src/api/routes/v1/index.ts
+++ src/api/routes/v1/index.ts
@@ -2,8 +2,6 @@ import {Request, Response, Router} from "express";
 import {router as AuthRouter} from "./auth.route";
 import {router as UserRouter} from "./user.route";
 import {router as DocumentRouter} from "./document.route";
-import {router as SettingRouter} from "./setting.route";
-
 export const router = Router();
 
 router.get("/status", (req: Request, res: Response) => {
@@ -24,7 +22,3 @@ router.use("/users/", UserRouter);
  * Files routes
  */
 router.use("/documents/", DocumentRouter);
-/**
- * Setting routes
-**/
-router.use('/settings', SettingRouter);
