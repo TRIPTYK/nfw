27bcff4cd3c0a8aa09ca1717a154074f70c35b89
amauryd <deflorenne.amaury@gmail.com>
2020-04-23 16:53:42 +0200

    nice comment

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,3 +1,7 @@
+/**
+ * Environement file is now dynamic to the API , no need to a CLI to update it anymore
+ */
+
 import EnvironmentConfiguration from "./src/config/environment.config";
 import { TypeORMConfiguration } from "./src/config/typeorm.config";
 
