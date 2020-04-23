d462f5601cdd27e4167ed52006768273fae91ea8
amauryd <deflorenne.amaury@gmail.com>
2020-04-23 18:46:00 +0200

    push error

--- src/api/controllers/status.controller.ts
+++ src/api/controllers/status.controller.ts
@@ -5,6 +5,7 @@ import { Request , Response } from "express";
 export default class StatusController {
     @Get("/")
     public status(req: Request, res: Response): void {
+        let unused = null;
         res.sendStatus(200);
     }
 }
