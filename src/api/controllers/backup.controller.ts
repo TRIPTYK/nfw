251b8738e4b35ea86a289fb62772fde7c496a10d
Stagiaires <teaflex.dev@hotmail.com>
2021-03-19 16:10:19 +0000

    oops forgot that

---
+++ src/api/controllers/backup.controller.ts
@@ -0,0 +1,52 @@
+import { ApplicationLifeCycleEvent, ApplicationRegistry, BaseController, Controller, Post } from "@triptyk/nfw-core";
+import { autoInjectable } from "tsyringe";
+import * as SocketIO from "socket.io-client";
+import { Request, Response } from "express";
+import httpStatus from "http-status";
+
+@Controller("backup")
+@autoInjectable()
+export class BackupController extends BaseController {
+
+    private socket = null;
+
+    constructor() {
+        super();
+        this.socket = SocketIO("http://localhost:3000", {
+			query: {
+				app: false,
+			},
+		});
+		ApplicationRegistry.on(ApplicationLifeCycleEvent.Running, () => {
+			this.socket.on("connect", () => {
+				this.socket.emit("hello");
+			});
+		});
+    }
+
+    @Post()
+    public async restore(req: Request, res: Response) {
+        await this.sendMessageAndWaitResponse("restore-backup");
+        res.status(200).send();
+        await this.sendMessageAndWaitResponse("app-recompile-sync");
+		await this.sendMessageAndWaitResponse("app-restart");
+    }
+
+    @Post()
+    public async save(req: Request, res: Response) {
+        await this.sendMessageAndWaitResponse("app-save");
+        res.status(200).send();
+    }
+
+    private sendMessageAndWaitResponse(type: string, data?: any) {
+		return new Promise((resolve, rej) => {
+			this.socket.emit(type, data, (response) => {
+				if (response !== "ok") {
+					rej(response);
+				} else {
+					resolve(response);
+				}
+			});
+		});
+	}
+}
