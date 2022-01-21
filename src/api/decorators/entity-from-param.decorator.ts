b19efb61d8a648a16b5a9e4e4d1c73da7ea86862
AmauryD <deflorenne.amaury@gmail.com>
2022-01-21 13:46:07 +0100

    Fix links

--- src/api/decorators/entity-from-param.decorator.ts
+++ src/api/decorators/entity-from-param.decorator.ts
@@ -1,5 +1,5 @@
-import { SqlEntityRepository } from '.pnpm/@mikro-orm+knex@4.5.9_357d0c55e72fe1e68b8c6f7ea14d2277/node_modules/@mikro-orm/knex';
 import { BaseEntity, MikroORM } from '@mikro-orm/core';
+import { SqlEntityRepository } from '@mikro-orm/mysql';
 import { Class, container, ControllerParamsContext, createCustomDecorator, databaseInjectionToken } from '@triptyk/nfw-core'
 
 export function EntityFromParam<K extends BaseEntity<any, any>> (param: keyof K, EntityModel: Class<K>) {
