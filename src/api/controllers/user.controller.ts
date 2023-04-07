e80846f461a32e25137af5f4cc7a38de8442e9dc
AmauryD <deflorenne.amaury@gmail.com>
2023-04-07 14:23:53 +0200

    up

--- src/api/controllers/user.controller.ts
+++ src/api/controllers/user.controller.ts
@@ -3,14 +3,11 @@ import { Body, Controller, DELETE, GET, Param, PATCH, POST, UseMiddleware } from
 import { CurrentUser } from '../decorators/current-user.decorator.js';
 import type { UserModel } from '../../database/models/user.model.js';
 import { CurrentUserMiddleware } from '../middlewares/current-user.middleware.js';
-import type { UserResourceAuthorizer } from '../resources/user/authorizer.js';
 import type { UserResourceDeserializer } from '../resources/user/deserializer.js';
 import type { UserResourceSerializer } from '../resources/user/serializer.js';
-import type { UserResourceValidator } from '../resources/user/validator.js';
 import type { ResourcesRegistry } from 'resources';
 import { assign, canOrFail } from 'resources';
 import type { UserResourceFactory } from '../resources/user/factory.js';
-import type { UserResourceAdapter } from '../resources/user/adapter.js';
 import { ResourcesRegistryImpl } from '@triptyk/nfw-resources';
 
 @singleton()
@@ -21,22 +18,14 @@ import { ResourcesRegistryImpl } from '@triptyk/nfw-resources';
 export class UsersController {
   private deserializer: UserResourceDeserializer;
   private serializer: UserResourceSerializer;
-  private authorizer: UserResourceAuthorizer;
-  private validator: UserResourceValidator;
   private factory: UserResourceFactory;
 
   public constructor (
     @inject(ResourcesRegistryImpl) public registry: ResourcesRegistry
   ) {
     this.deserializer = registry.getDeserializerFor('user') as UserResourceDeserializer;
-    this.validator = registry.getValidatorFor('user') as UserResourceValidator;
     this.serializer = registry.getSerializerFor('user') as UserResourceSerializer;
-    this.authorizer = registry.getAuthorizerFor('user') as UserResourceAuthorizer;
     this.factory = registry.getFactoryFor('user') as UserResourceFactory;
-  }
-
-  get adapter () {
-    return this.registry.getAdapterFor('user') as UserResourceAdapter
   }
 
   @GET('/:id')
@@ -44,8 +33,10 @@ export class UsersController {
       @Param('id') id: string,
       @CurrentUser() currentUser: UserModel
   ) {
-    const resource = await this.adapter.findById(id);
-    await this.authorizer.can(currentUser, 'read', resource);
+    const resource = await this.orm.findById(id);
+
+    // authorizer ...
+
     return this.serializer.serializeOne(resource);
   }
 
@@ -55,15 +46,6 @@ export class UsersController {
     @CurrentUser() currentUser: UserModel
   ) {
     const partialResource = await this.deserializer.deserialize(body);
-    const validated = await this.validator.validate(partialResource);
-    if (!validated.isValid) {
-      throw validated.errors;
-    }
-    const resource = await this.factory.create(
-      validated.result
-    );
-    await canOrFail(this.authorizer, currentUser, 'create', resource, {});
-    await this.adapter.create(resource);
     return this.serializer.serializeOne(resource);
   }
 
