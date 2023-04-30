68508c78523444662cd305edda6b80231ae4f3ec
AmauryD <deflorenne.amaury@gmail.com>
2023-04-30 16:58:02 +0200

    dup

--- config/vitest/unit.vitest.config.ts
+++ config/vitest/unit.vitest.config.ts
@@ -1,11 +1,9 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 import baseConfig from './base.config.js';
 
 export default defineConfig({
   ...baseConfig,
-  plugins: [tsconfigPaths()],
   test: {
     include: ['tests/src/unit/**/*.test.ts']
   }
