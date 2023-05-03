6d6173b90e8ae581082bab53a0aa55f9fc7de553
AmauryD <deflorenne.amaury@gmail.com>
2023-05-03 20:38:50 +0200

    try use param as class

--- src/api/decorators/current-user.decorator.ts
+++ src/api/decorators/current-user.decorator.ts
@@ -1,13 +1,20 @@
-import { container } from '@triptyk/nfw-core';
+import { inject, injectable } from '@triptyk/nfw-core';
+import type { ControllerParamsContext, ParamInterface } from '@triptyk/nfw-http';
 import { createCustomDecorator } from '@triptyk/nfw-http';
 import { UserService } from '../services/user.service.js';
 
-export function CurrentUser () {
-  return createCustomDecorator(async ({ ctx }) => {
-    const userService = container.resolve(UserService);
+@injectable()
+class CurrentUserParam implements ParamInterface<unknown> {
+  public constructor (
+    @inject(UserService) private userService: UserService
+  ) {}
 
-    const user = await userService.tryLoadUserFromToken(ctx.headers.authorization ?? '');
-
+  public async handle ({ ctx }: ControllerParamsContext<unknown>) {
+    const user = await this.userService.tryLoadUserFromToken(ctx.headers.authorization ?? '');
     return user;
-  }, 'current-user');
+  }
+}
+
+export function CurrentUser () {
+  return createCustomDecorator(CurrentUserParam, 'current-user');
 }
