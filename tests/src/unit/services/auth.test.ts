33d86bb8483c01e6d931f30878f0691d5ceab4da
AmauryD <deflorenne.amaury@gmail.com>
2023-05-09 12:45:11 +0200

    clear mocks due to vite update

--- tests/src/unit/services/auth.test.ts
+++ tests/src/unit/services/auth.test.ts
@@ -1,9 +1,17 @@
-import { hash } from 'bcrypt';
 import 'reflect-metadata';
-import { afterEach, expect, test, vi, beforeEach } from 'vitest';
+import { expect, test, vi, beforeEach, afterAll, beforeAll } from 'vitest';
+import { hash } from 'bcrypt';
 import { AuthService } from '../../../../src/api/services/auth.service.js';
 
 let service: AuthService;
+
+const BCRYPT_RETURN_VALUE = '';
+
+vi.mock('bcrypt', () => {
+  return {
+    hash: vi.fn(() => BCRYPT_RETURN_VALUE)
+  }
+})
 
 const mockTokenRepository = {
   generateRefreshToken: vi.fn()
@@ -26,29 +34,15 @@ const configurationService = {
 
 beforeEach(() => {
   service = new AuthService(mockTokenRepository as never, configurationService as never);
-  vi.useFakeTimers()
 });
 
-afterEach(() => {
-  vi.useRealTimers();
-  vi.restoreAllMocks();
+beforeAll(() => {
+  vi.useFakeTimers();
 })
 
-test('access token generation from configuration', () => {
-  const date = new Date(949406400000);
-  vi.setSystemTime(date)
-
-  const token = service.generateAccessToken('123');
-
-  expect(token).toStrictEqual('eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjk0OTQxMDAwMCwiaWF0Ijo5NDk0MDY0MDAsInN1YiI6IjEyMyIsIm5iZiI6OTQ5NDA2Mzk5LCJhdWQiOiJ0cmlwdHlrLmV1IiwiaXNzIjoidHJpcHR5ay5ldSJ9.E3JGmiHu3nmaPGkshxNnjoXGc60CXX0zx7kczhSlin-SlbQXEIyMU4pa1g0ZGL1X53MmtSsczjHxeLQgeDiJcA');
-});
-
-const BCRYPT_RETURN_VALUE = '';
-
-vi.mock('bcrypt', () => {
-  return {
-    hash: vi.fn(() => BCRYPT_RETURN_VALUE)
-  }
+afterAll(() => {
+  vi.useRealTimers();
+  vi.resetAllMocks();
 })
 
 test('password hashing', async () => {
@@ -58,4 +52,13 @@ test('password hashing', async () => {
   const token = await service.hashPassword(passwordToHash);
   expect(token).toStrictEqual(BCRYPT_RETURN_VALUE);
   expect(hash).toBeCalledWith(passwordToHash, expectedRounds);
+});
+
+test('access token generation from configuration', () => {
+  const date = new Date(949406400000);
+  vi.setSystemTime(date)
+
+  const token = service.generateAccessToken('123');
+
+  expect(token).toStrictEqual('eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjk0OTQxMDAwMCwiaWF0Ijo5NDk0MDY0MDAsInN1YiI6IjEyMyIsIm5iZiI6OTQ5NDA2Mzk5LCJhdWQiOiJ0cmlwdHlrLmV1IiwiaXNzIjoidHJpcHR5ay5ldSJ9.E3JGmiHu3nmaPGkshxNnjoXGc60CXX0zx7kczhSlin-SlbQXEIyMU4pa1g0ZGL1X53MmtSsczjHxeLQgeDiJcA');
 });
